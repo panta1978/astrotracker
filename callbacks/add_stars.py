42679777c471793bb49639611bf6eebbb9f8bcc8
panta1978 <stefano.pantaleoni@gmail.com>
2025-11-20 06:49:31 +0100

    Add descriptions to Add Locations and Add Stars UI

--- callbacks/add_stars.py
+++ callbacks/add_stars.py
@@ -49,57 +49,61 @@ class AddStarDialog(QDialog):
         grid.setColumnStretch(6, 2)
         layout.addLayout(grid)
 
+        # --- Row 0: Label
+        descr_label = 'Search for a star. Use English names (e.g. <b>Arcturus</b>)'
+        grid.addWidget(QLabel(descr_label), 0, 0, 1, 7)
+
         # --- Row 1: Star Name + Check
-        grid.addWidget(QLabel('Star Name'), 0, 0, 1, 1)
+        grid.addWidget(QLabel('Star Name'), 1, 0, 1, 1)
         self.star_name_field = QLineEdit()
-        grid.addWidget(self.star_name_field, 0, 1, 1, 4)
+        grid.addWidget(self.star_name_field, 1, 1, 1, 4)
 
         self.check_btn = QPushButton('Check')
         self.check_btn.clicked.connect(lambda: ascb.check_star(self))
-        grid.addWidget(self.check_btn, 0, 5, 1, 1)
+        grid.addWidget(self.check_btn, 1, 5, 1, 1)
 
         self.check_result_field = QLineEdit()
         self.check_result_field.setReadOnly(True)
         self.check_result_field.setStyleSheet('background-color: #e8e8e8; border: 1px solid #cccccc')
-        grid.addWidget(self.check_result_field, 0, 6, 1, 1)
+        grid.addWidget(self.check_result_field, 1, 6, 1, 1)
 
         # --- Row 2: RA / Dec ---
-        grid.addWidget(QLabel('RA2000'), 1, 0, 1, 1)
+        grid.addWidget(QLabel('RA2000'), 2, 0, 1, 1)
         self.ra_field = QLineEdit()
         self.ra_field.setReadOnly(True)
         self.ra_field.setStyleSheet('background-color: #e8e8e8; border: 1px solid #cccccc')
-        grid.addWidget(self.ra_field, 1, 1, 1, 2)
+        grid.addWidget(self.ra_field, 2, 1, 1, 2)
 
-        grid.addWidget(QLabel('Dec2000'), 1, 4, 1, 1)
+        grid.addWidget(QLabel('Dec2000'), 2, 4, 1, 1)
         self.dec_field = QLineEdit()
         self.dec_field.setReadOnly(True)
         self.dec_field.setStyleSheet('background-color: #e8e8e8; border: 1px solid #cccccc')
-        grid.addWidget(self.dec_field, 1, 5, 1, 2)
+        grid.addWidget(self.dec_field, 2, 5, 1, 2)
 
         # --- Row 3: pm_ra / pm_dec ---
-        grid.addWidget(QLabel('pm_ra'), 2, 0, 1, 1)
+        grid.addWidget(QLabel('pm_ra'), 3, 0, 1, 1)
         self.pm_ra_field = QLineEdit()
         self.pm_ra_field.setReadOnly(True)
         self.pm_ra_field.setStyleSheet('background-color: #e8e8e8; border: 1px solid #cccccc')
-        grid.addWidget(self.pm_ra_field, 2, 1, 1, 2)
+        grid.addWidget(self.pm_ra_field, 3, 1, 1, 2)
 
-        grid.addWidget(QLabel('pm_dec'), 2, 4, 1, 1)
+        grid.addWidget(QLabel('pm_dec'), 3, 4, 1, 1)
         self.pm_dec_field = QLineEdit()
         self.pm_dec_field.setReadOnly(True)
         self.pm_dec_field.setStyleSheet('background-color: #e8e8e8; border: 1px solid #cccccc')
-        grid.addWidget(self.pm_dec_field, 2, 5, 1, 2)
+        grid.addWidget(self.pm_dec_field, 3, 5, 1, 2)
 
         # --- Row 4: Vizier Name and buttons ---
-        grid.addWidget(QLabel('Vizier Name'), 3, 0)
+        grid.addWidget(QLabel('Vizier Name'), 4, 0)
         self.vizier_field = QLineEdit()
         self.vizier_field.setReadOnly(True)
         self.vizier_field.setStyleSheet('background-color: #e8e8e8; border: 1px solid #cccccc')
-        grid.addWidget(self.vizier_field, 3, 1, 1, 2)
+        grid.addWidget(self.vizier_field, 4, 1, 1, 2)
 
         self.update_btn = QPushButton('Update DB')
         self.update_btn.clicked.connect(lambda: ascb.update_db(self))
         self.update_btn.setEnabled(False)
-        grid.addWidget(self.update_btn, 3, 5, 1, 1)
+        grid.addWidget(self.update_btn, 4, 5, 1, 1)
         self.close_btn = QPushButton('Close')
         self.close_btn.clicked.connect(self.close)
         grid.addWidget(self.close_btn, 3, 6, 1, 1)
